5da6aeb545d9b800b5f082c588c48e1643f68a24
Ashik Mridha <ashikmridha43@gmail.com>
2026-05-20 14:41:51 +0600

    get an idea page

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express');
 const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT;
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -20,14 +24,36 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        
+
         await client.connect();
-        
+
+        const db = client.db("ideaVault");
+        const ideaCollection = db.collection("ideas");
+
+        app.get('/ideas', async (req, res) => {
+            const result = await ideaCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        app.post('/ideas', async (req, res) => {
+            const ideaData = req.body;
+            console.log(ideaData);
+            const result = await ideaCollection.insertOne(ideaData);
+            res.send(result);
+        });
+
+        app.get('/idea/:id', async (req, res) => {
+            const id = req.params;
+            const result = await ideaCollection.findOne({ _id: new ObjectId(id) })
+            res.json(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        
-        await client.close();
+
+        // await client.close();
     }
 }
 run().catch(console.dir);
